b80a775b9564ed5cda9a62f530b1506d728d6161
mtomison <42246413+mtomison@users.noreply.github.com>
2021-09-12 10:51:10 -0500

    Final final commit

---
+++ Vacation_Search/config.py
@@ -0,0 +1,4 @@
+weather_api_key="d5fa55051f7d5202d29d7715edfc3ace"
+g_key='AIzaSyAZYb1Y0P2bdLPNRCLHg8_k-dQWGvc1wu8'
+
+# api.openweathermap.org/data/2.5/weather?q={Pittsburgh}&appid={weather_api_key}
